6a71602a61e5bec21b0f6918f1732da7e4b9b5dc
ahgddev <ahgddev@gmail.com>
2024-01-25 16:21:12 -0700

    Cleaned up bugs with index.html and index.js.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-import "./styles.css";
+let mainEl = document.getElementsByTagName("main")
+mainEl[0].style.backgroundColor = "var(--main-bg)"
+mainEl[0].innerHTML = "<h1>DOM Manipulation</h1>"
+mainEl[0].classList.add("flex-ctr");
+
+let topMenuEl = document.getElementById("top-menu")
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around")
+
+
 // Menu data structure
 var menuLinks = [
     { text: 'about', href: '/about' },
@@ -8,18 +18,9 @@ var menuLinks = [
   ];
 
 //Create Nav
-for (link in menuLinks){
+for (link of menuLinks){
    let newLink = topMenuEl.appendChild(document.createElement("a"))
    newLink.href = link.href
    newLink.innerText = link.text
+   console.log(link.href, link.text)
 }
-
-let mainEl = document.getElementsByTagName("main")
-mainEl.style.backgroundColor = "var(--sub-menu-bg)"
-mainEl.appendChild(document.createElement("h1").innerHTML="DOM Manipulation")
-mainEl.classList.add("flex-ctr");
-
-let topMenuEl = document.getElementById("top-menu")
-topMenuEl.style.height = "100%";
-topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
-topMenuEl.classList.add("flex-around")
